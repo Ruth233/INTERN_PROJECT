bb833134a46a679b944665833fdaa2bae33c293f
Laud Tetteh <kobbykurtx@gmail.com>
2025-09-12 12:28:57 +0000

    feat: add NSS type definition with nssID and email fields

---
+++ src/Types/nss.ts
@@ -0,0 +1,12 @@
+export interface Nss {
+  id: number;
+  name: string;
+  phone: string;
+  nssID: string;
+  email: string;
+  institution: string;
+  course: string;
+  interest: string;
+  startDate: string;
+  endDate: string;
+}
