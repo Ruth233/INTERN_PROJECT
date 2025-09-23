4451a0bd7e3f5b15ce14648b7b66cfca6969bc7d
Laud Tetteh <kobbykurtx@gmail.com>
2025-09-23 20:52:39 +0000

    Enhance AppLayout component with improved navigation and layout structure

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,14 +1,23 @@
 import { Outlet } from "react-router-dom";
 import { NavLink, useNavigate } from "react-router-dom";
+import { logout } from "../api";
 import { FiLogOut } from "react-icons/fi";
 
 const AppLayout = () => {
   const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logout();
+    } catch {
+      alert("Failed to logout");
+    }
+    navigate("/", { replace: true });
+  };
   return (
     <div>
       <div className="flex items-center justify-between fixed z-10 py-2 px-4 bg-white w-full top-0 left-0 border border-b-gray-300 shadow-md">
         <button
-          onClick={() => navigate("/")}
+          onClick={handleLogout}
           className="py-1 px-3 bg-blue-950 rounded-md cursor-pointer text-white flex items-center gap-2"
         >
           <FiLogOut />
